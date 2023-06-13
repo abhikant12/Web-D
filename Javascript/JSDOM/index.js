719afe5dcefd0ab3e6c97ea0ef51bc60455dd7c1
Abhikant kumar <abhiparnav12@gmail.com>
2023-06-13 16:18:25 +0530

    added synchronous and unsynchronous function

--- Javascript/JSDOM/index.js
+++ Javascript/JSDOM/index.js
@@ -102,6 +102,152 @@ console.log("this took " + (t6-t5) + " ms");
 
 
 
+/*
+JavaScript is an asynchronous and concurrent programming language that offers a lot of flexibility. It’s single-threaded like sync, 
+but also non-blocking like async.
+
+Although it’s synchronous by nature, JavaScript benefits from asynchronous code. Long-running JavaScript functions can make the user 
+interface (UI) or server unresponsive until the function has returned, resulting in a less-than-stellar user experience. However,
+there are some instances where users can benefit from blocking programming; for example, when making an online payment.
+
+The beauty of JavaScript is that it offers the best of both worlds: Single-thread and multi-thread, blocking and non-blocking.
+With this flexibility, programmers can write code in a single programming language instead of two—one for synchronous operations and 
+another for asynchronous operations.
+
+Asynchronous is a non-blocking architecture, so the execution of one task isn’t dependent on another. Tasks can run simultaneously. 
+Synchronous is a blocking architecture, so the execution of each operation is dependent on the completion of the one before it. Each 
+task requires an answer before moving on to the next iteration.
+*/
+
+/*
+setTimeout(function(){                                //asynchronous code  , output :-  first,second , third
+    console.log('third');
+},3000)
+
+function sync() {
+    console.log('first');
+}
+sync();
+console.log('second');
+*/
+
+/*
+let meraPromise1 = new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            console.log('I am insde Promise1');
+        }, 5000);
+        //resolve(1234567890);
+        reject(new Error('Bhaisahab Error aaaye hai'))
+});
+meraPromise1.then((value) => { console.log(value)}, (error) => { console.log("Recieved an Error")});   // commnet it to see its working;
+
+
+let meraPromise2 = new Promise(function(resolve, reject) {
+    setTimeout(function() {
+        console.log('I am insde Promise2');
+    }, 3000);
+    //resolve(2233);
+    //reject(new Error('Bhaisahab Error aaaye hai'))
+});
+console.log('Pehla');
+*/
+
+ /*
+let waadaa1 = new Promise(function(resolve, reject) {
+    setTimeout(()=> {
+        console.log('setimeout1 started');                             //it print after 2 sec;
+    },2000);
+    resolve(true);
+})
+
+let output = waadaa1.then(() => {
+    let waadaa2 = new Promise(function(resolve, reject) {
+        setTimeout(()=> {
+            console.log('setimeout2 started');                            //it print after 3 sec;
+        },3000);
+        resolve("waada 2 resolved");
+    })
+    return waadaa2;
+})
+output.then((value) => console.log(value));                        // output return resolve or reject ;
+
+*/
+
+/*
+async function abcd() {                                       // it return promise "kya hua tera";                    
+    return "kya hua tera";
+}
+
+async function utility() {                                     //this async function means both promise run parallely      
+
+      let delhiMausam = new Promise((resolve, reject) => {
+          setTimeout(()=>{
+              resolve("Delhi me bhot garmi hai");
+          },5000);
+      });
+
+      let hydMausam = new Promise((resolve, reject) => {
+          setTimeout(()=>{
+              resolve("Hyderabad is Cool");
+          },6000);
+      });
+
+      let dM =  delhiMausam;                                   //if we want to make sync then attach await keyword like  let dM = await delhiMausam; 
+      let hM =  hydMausam;
+
+      return [dM, hM];
+ }
+
+//await keyword is used when we want to  run that line after excution of above code;
+async function utility() {
+    let content = await fetch('https://jsonplaceholder.typicode.com/posts/1');                 // it fetch the content;
+    let output = await content.json();                                                         // converted into json(javascript object notation)
+    console.log(content);
+}
+utility();
+
+
+ */
+
+/*
+async function helper() {
+
+    let options = {                                                   //these are data;
+        method: 'POST',
+        body: JSON.stringify({
+          title: 'Babbar',
+          body: 'Tagdi Body ',
+          userId: 1998,
+          weight: 90,
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+    };
+    
+    let content = await fetch('https://jsonplaceholder.typicode.com/posts', options);          //we are sending the data at this url;
+    let response = content.json();
+    return response;
+}
+
+async function utility() {
+    let ans = await helper();
+    console.log(ans);
+}
+utility();
+ 
+function init() {                                                     // closer function  , when there is closer nested function then data pass in reference;
+    let name = "Mozilla";  
+    function displayName() {                                         // displayName() is the inner function, that forms the closure
+      console.log(name);                                             // use variable declared in the parent function
+    }
+    return displayName;
+}
+let a = init();
+a();
+  
+*/
+
 
  
 
