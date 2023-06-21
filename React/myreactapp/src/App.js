9fa0ecf2454fde29cfe88e47450cf1dfc90f800e
Abhikant kumar <abhiparnav12@gmail.com>
2023-06-21 10:35:33 +0530

    added demo react file

---
+++ React/myreactapp/src/App.js
@@ -0,0 +1,13 @@
+ import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='App-header'>
+         Abhikant Singh
+      </div>
+    </div>
+  );
+}
+
+export default App;
